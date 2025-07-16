19d770950897519733677aa109669497adea84d3
dharmateja03 <59060125+dharmateja03@users.noreply.github.com>
2025-07-16 13:26:52 -0400

    Update maxSubArray.py

--- maxSubArray.py
+++ maxSubArray.py
@@ -2,7 +2,7 @@
 # SpaceComplexity: constant
 # Approach: at every number you have 2 choices either start sum with that number or let the number be part of subarry but as we need max we update our running sum only if rsum+nums[i] is less
 # than nums[i]
-# Brute force would be find all subarrays O(n!)
+# Brute force would be find all subarrays O(n^3)
 # to find index you might thing to update start and end when rsuma dn max gets upadated but thats not tottaly correct for rsum get updated evry time when nums[i] is greater than sum
 # which is local but we need global so we have curr start and start (for global)/
 
